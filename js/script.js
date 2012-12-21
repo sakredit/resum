182455c25c36cd00072e1c2d435b9ae3af75667b
Alex <sakredit@gmail.com>
2012-12-21 23:28:20 +0200

    sdv
    sdv

---
+++ js/script.js
@@ -0,0 +1,18 @@
+/* Author:
+
+*/
+
+$(document).ready(function(){
+ $.fn.animate_Text = function() {
+  var string = this.text();
+  return this.each(function(){
+   var $this = $(this);
+   $this.html(string.replace(/./g, '<span class="new">$&</span>'));
+   $this.find('span.new').each(function(i, el){
+    setTimeout(function(){ $(el).addClass('div_opacity'); }, 60 * i);
+   });
+  });
+ };
+ $('#example').show();
+ $('#example').animate_Text();
+});
